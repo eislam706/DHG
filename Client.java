59f2df97552cc9469e4bb82ac5f575ff42e05f5f
Ekramul Islam <eislam706@gmail.com>
2020-10-21 18:29:22 +0600

    initial commit

---
+++ Client.java
@@ -0,0 +1,143 @@
+// Java implementation for multithreaded chat client 
+// Save file as Client.java 
+
+import java.io.*; 
+import java.util.*; 
+import java.net.*; 
+import java.util.Scanner; 
+import java.math.BigInteger; 
+import java.security.MessageDigest; 
+import java.security.NoSuchAlgorithmException;
+
+public class Client 
+{ 
+	final static int ServerPort = 1234; 
+
+	public static void main(String args[]) throws UnknownHostException, IOException 
+	{ 
+		Scanner scn = new Scanner(System.in); 
+		
+		// getting localhost ip 
+		InetAddress ip = InetAddress.getByName("localhost"); 
+		
+		// establish the connection 
+		Socket s = new Socket(ip, ServerPort); 
+		
+		// obtaining input and out streams 
+		DataInputStream dis = new DataInputStream(s.getInputStream()); 
+		DataOutputStream dos = new DataOutputStream(s.getOutputStream()); 
+
+		// sendMessage thread 
+		Thread sendMessage = new Thread(new Runnable() 
+		{ 
+			@Override
+			public void run() { 
+				while (true) { 
+
+					// read the message to deliver. 
+					String msg = scn.nextLine(); 
+					
+					try { 
+						// write on the output stream 
+						dos.writeUTF(msg); 
+					} catch (IOException e) { 
+						e.printStackTrace(); 
+					} 
+				} 
+			} 
+		}); 
+		
+		// readMessage thread 
+		Thread readMessage = new Thread(new Runnable() 
+		{ 
+			@Override
+			public void run() { 
+
+				while (true) { 
+					try { 
+						// read the message sent to this client 
+						String msg = dis.readUTF(); 
+						System.out.println(msg); 
+						boolean b = checking(msg);
+						dos.writeUTF(b?"1":"0");
+					} catch (IOException e) { 
+
+						e.printStackTrace(); 
+					} 
+				} 
+			} 
+		}); 
+
+		sendMessage.start(); 
+		readMessage.start(); 
+	} 
+
+	private static boolean checking(String msg) {
+		StringTokenizer st = new StringTokenizer(msg, "#"); 
+		String t = st.nextToken();
+		int blockNo = Integer.parseInt(st.nextToken());
+		int b = blockNo;
+		
+		int ii = b*4194304;
+
+		for(;ii<4194304*(b+1);ii++) {
+			blockNo = ii;
+
+			// System.out.println(t+" "+blockNo);
+			String s = "37";
+			// String s = "00";
+
+			if (t.length()<4){
+				for(int i=0;i<4-t.length();i++){
+				t="0"+t;
+				}
+			}
+			s=s+t.substring(t.length()-3);
+			String v = t+""+blockNo;
+			// System.out.println(v);
+			// System.out.println(s);
+
+			String m = getMd5(v);
+			// System.out.println(m);
+
+			String firstFive = m.substring(0, 5);
+
+
+			// System.out.println(firstFive);
+			System.out.println(blockNo);
+
+			if(firstFive.equals(s)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static String getMd5(String input) 
+    { 
+        try { 
+  
+            // Static getInstance method is called with hashing MD5 
+            MessageDigest md = MessageDigest.getInstance("MD5"); 
+  
+            // digest() method is called to calculate message digest 
+            //  of an input digest() return array of byte 
+            byte[] messageDigest = md.digest(input.getBytes()); 
+  
+            // Convert byte array into signum representation 
+            BigInteger no = new BigInteger(1, messageDigest); 
+  
+            // Convert message digest into hex value 
+            String hashtext = no.toString(16); 
+            while (hashtext.length() < 32) { 
+                hashtext = "0" + hashtext; 
+            } 
+            return hashtext; 
+        }  
+  
+        // For specifying wrong message digest algorithms 
+        catch (NoSuchAlgorithmException e) { 
+            throw new RuntimeException(e); 
+        } 
+    }
+} 
---
+++ Server.java
@@ -0,0 +1,177 @@
+import java.io.*; 
+import java.util.*; 
+import java.net.*; 
+import java.util.Scanner; 
+import java.util.ArrayList;
+import java.util.List;
+
+// Server class 
+public class Server 
+{ 
+
+	// Vector to store active clients 
+	static Vector<ClientHandler> ar = new Vector<>(); 
+	
+	// counter for clients 
+	static int i = 0; 
+	static String roll = "";
+	static List<Integer> blocks = new ArrayList<>();
+
+	public static void main(String[] args) throws IOException 
+	{ 
+		// server is listening on port 1234 
+		ServerSocket ss = new ServerSocket(1234); 
+        Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter roll no:");
+		roll = scan.nextLine();
+		
+		Socket s; 
+		
+		// running infinite loop for getting 
+		// client request 
+		while (true) 
+		{ 
+			// Accept the incoming request 
+			s = ss.accept(); 
+
+			System.out.println("New client request received : " + s); 
+			
+			// obtain input and output streams 
+			DataInputStream dis = new DataInputStream(s.getInputStream()); 
+			DataOutputStream dos = new DataOutputStream(s.getOutputStream()); 
+			
+			System.out.println("Creating a new handler for this client..."); 
+
+			// Create a new handler object for handling this request. 
+			ClientHandler mtch = new ClientHandler(s, "c" + i, i, dis, dos); 
+
+			// Create a new Thread with this object. 
+			Thread t = new Thread(mtch); 
+			
+			System.out.println("Adding this client to active client list"); 
+
+			// add this client to active clients list 
+			ar.add(mtch); 
+
+			// start the thread. 
+			t.start(); 
+
+			sendClientRoll(i, dos);
+
+			// increment i for new client. 
+			// i is used for naming only, and can be replaced 
+			// by any naming scheme 
+			i++; 
+
+		} 
+	} 
+
+	private static void sendClientRoll(int id, DataOutputStream dos) throws IOException {
+		String name = "c"+i;
+		int sizB = blocks.size();
+		sizB = sizB==0?0:sizB-1;
+		for (ClientHandler mc : ar) 
+		{ 
+			if (mc.name.equals(name) && mc.isloggedin==true) 
+			{ 
+				blocks.add(i);
+				dos.writeUTF(roll+"#"+(blocks.size()-1));
+				System.out.println(mc.name+"#"+(blocks.size()-1)); 
+				break; 
+			} 
+		}
+	}
+} 
+
+// ClientHandler class 
+class ClientHandler implements Runnable 
+{ 
+	Scanner scn = new Scanner(System.in); 
+	public String name; 
+	final DataInputStream dis; 
+	public final DataOutputStream dos; 
+	Socket s; 
+	boolean isloggedin;
+	public int id;
+	
+	// constructor 
+	public ClientHandler(Socket s, String name, int id,
+							DataInputStream dis, DataOutputStream dos) { 
+		this.dis = dis; 
+		this.dos = dos; 
+		this.name = name; 
+		this.s = s; 
+		this.isloggedin=true;
+		this.id = id;
+	} 
+
+	@Override
+	public void run() { 
+
+		String received; 
+		while (true) 
+		{ 
+			try
+			{
+				// receive the string 
+				received = dis.readUTF(); 
+				
+				System.out.println(received); 
+				
+				if(received.equals("logout")){ 
+					this.isloggedin=false; 
+					this.s.close(); 
+					break; 
+				} 
+
+				int checkValue = Integer.parseInt(received);
+				if(checkValue == 0) {
+					for (ClientHandler mc : Server.ar) 
+					{ 
+						if (mc.name.equals(this.name) && mc.isloggedin==true) 
+						{ 
+							if(Server.blocks.size()==1023){
+								break;
+							}
+							mc.dos.writeUTF(Server.roll+"#"+(Server.blocks.size()-1)); 
+							Server.blocks.add(mc.id);
+							System.out.println(mc.name+"#"+(Server.blocks.size()-1));
+							break; 
+						} 
+					} 
+				}
+			
+				
+				// break the string into message and recipient part 
+				// StringTokenizer st = new StringTokenizer(received, "#"); 
+				// String MsgToSend = st.nextToken(); 
+				// String recipient = st.nextToken(); 
+
+				// System.out.println(Server.roll);
+
+				// for (ClientHandler mc : Server.ar) 
+				// { 
+					// if (mc.name.equals(this.name) && mc.isloggedin==true) 
+					// { 
+						// mc.dos.writeUTF(this.name+" : "+MsgToSend); 
+						// break; 
+					// } 
+				// } 
+
+			} catch (IOException e) { 
+				
+				e.printStackTrace(); 
+			} 
+			
+		} 
+		try
+		{ 
+			// closing resources 
+			this.dis.close(); 
+			this.dos.close(); 
+			
+		}catch(IOException e){ 
+			e.printStackTrace(); 
+		} 
+	} 
+} 
